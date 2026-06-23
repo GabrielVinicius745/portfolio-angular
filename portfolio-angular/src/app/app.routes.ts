c7a380605807d5cf57e5842e55ff834d910e6c0f
GabrielVinicius745 <20241CTB0100024@estudantes.ifpr.edu.br>
2026-06-23 02:39:37 +0000

    Aula17: API de projetos e catalogo (MariaDB + JSON)

--- portfolio-angular/src/app/app.routes.ts
+++ portfolio-angular/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Home } from './home/home';
 import { Sobre } from './sobre/sobre';
 import { Projetos } from './projetos/projetos';
 import { Contato } from './contato/contato';
+import { Login } from './login/login';
 
 export const routes: Routes = [
     { path: '', component: Home },
     { path: 'sobre', component: Sobre },
     { path: 'projetos', component: Projetos },
-    { path: 'contato', component: Contato }
+    { path: 'contato', component: Contato },
+    { path: 'login', component: Login }
 ];
 
