48d676d86b9cd20408c32d686993fed4d34daf15
GabrielVinicius745 <20241CTB0100024@estudantes.ifpr.edu.br>
2026-09-22 14:32:53 +0000

    Aula18: formulario de Contato com POST e API (Reactive Forms + validacao)

---
+++ portfolio-angular/src/app/contato.service.ts
@@ -0,0 +1,18 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+export interface NovoContato { 
+    nome: string; email: string; mensagem: string;
+}
+export interface RespostaContato {
+    sucesso: boolean; id: number; mensagem: string;
+}
+
+@Injectable({ providedIn: 'root' })
+export class ContatoService {
+    private http = inject(HttpClient);
+    private url = 'https://ideal-dollop-4j4rwr5gjp99fq7wv-8000/api/contato.php';
+    enviar(dados: NovoContato): Observable<RespostaContato> {
+        return this.http.post<RespostaContato>(this.url, dados);
+    }
+}
--- portfolio-angular/src/app/contato/contato.ts
+++ portfolio-angular/src/app/contato/contato.ts
@@ -1,9 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { ContatoService, NovoContato } from '../contato.service';
 
 @Component({
   selector: 'app-contato',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './contato.html',
-  styleUrl: './contato.scss',
 })
-export class Contato {}
+export class Contato {
+  private fb = inject(FormBuilder);
+  private contatoService = inject(ContatoService);
+  enviando = false; sucesso = false; erro = '';
+
+  form = this.fb.group({
+    nome: ['', [Validators.required, Validators.minLength(3)]],
+    email: ['', [Validators.required, Validators.email]],
+    mensagem: ['', [Validators.required, Validators.minLength(10)]],
+  });
+
+  onSubmit() {
+    this.sucesso = false; this.erro = '';
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+    this.enviando = true;
+    this.contatoService.enviar(this.form.value as NovoContato).subscribe({
+      next: () => {
+        this.enviando = false;
+        this.sucesso = true;
+        this.form.reset();
+      },
+      error: () => {
+        this.erro = 'Nao foi possivel enviar. Tente novamente.';
+        this.enviando = false;
+      },
+    });
+  } 
+}
